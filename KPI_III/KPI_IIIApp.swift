739da44816db470168f025899b08a81f8dddb59b
Abdurakhmon Jamoliddinov <jamoliddinovabdu@gmail.com>
2023-04-26 23:29:07 +0500

    Initial Commit

---
+++ KPI_III/KPI_IIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPI_IIIApp.swift
+//  KPI_III
+//
+//  Created by Workspace (Abdurakhmon Jamoliddinov) on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct KPI_IIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
